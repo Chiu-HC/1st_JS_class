9f61c8565b8c18a4aae0c1b6fbda2895df86692d
HC <test@user.co.tw>
2026-09-06 15:44:30 +0800

    Del same txt

---
+++ 20260906/2_script.js
@@ -0,0 +1,11 @@
+        // 註解就是使程式碼無效
+        console.log("hello world");
+        /* console.log(50*30+50);
+          console.log(50*30+50);*/
+        const num=50 * 30 + 15; // 將右邊結果存入變數num裡面
+        /*console.log(num);
+        console.warn(num);
+        console.error(num);
+        console.info(num);*/
+
+        document.querySelector("h1").innerText = "您好"; // 將變數num的值放入id為demo的標籤裡面
